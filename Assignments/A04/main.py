82552d771a481814bffe9a9f99c2ca0502b04c1c
Landen Jones <59664899+LandenSJones@users.noreply.github.com>
2020-07-09 15:13:27 -0500

    Update main.py

--- Assignments/A04/main.py
+++ Assignments/A04/main.py
@@ -1,24 +1,66 @@
+#Landen Jones
+#07/09/2020
 import tkinter as tk
+import sys
+import os
+import json
 
 # GUI window is a subclass of the basic tkinter Frame object
-class HelloWorldFrame(tk.Frame):
-    def __init__(self, master,name):
+class MainFrame(tk.Frame):
+    def __init__(self, master):
         # Call superclass constructor
         tk.Frame.__init__(self, master)
         # Place frame into main window
-        self.grid()
-        # Create text box with "Hello World" text
-        hello = tk.Label(self, text=f"Hello World! My name is {name}!")
-        # Place text box into frame
-        hello.grid(row=0, column=0)
+        self.grid()        
+        
+        #Get the name of the JsonFile
+        jsonFile = sys.argv[1]
+
+        #Checks to see if the file is available
+        if os.path.isfile(jsonFile):
+            infile = open(jsonFile, "r")
+            data = infile.read()
+            infile.close
+        else:
+            print("Error: File doesn't exist!")
+        #Loads data into a dictionary from Json data
+        playerInfo = json.loads(data)
+
+        #J is the value of the row to be printed on
+        j = 0
+        #For loop to traverse each dictionary pair and we print to window :)
+        for key, value in playerInfo.items():
+            #Skip the Screen_Name b/c it is only useful for the window name
+            if key != "screen_name":
+                result = str(key).ljust(15)
+                text = tk.Label(self, text= result, justify= tk.LEFT, anchor="w").grid(sticky = tk.W, column=0,row=j)
+                result = ":   " + str(value).strip("[]")
+                result = result.replace("'","")
+                text = tk.Label(self, text= result, justify= tk.LEFT, anchor="w").grid(sticky = tk.W, column=25,row=j)
+                j+=1
 
 # Spawn window
 if __name__ == "__main__":
+
+    #--------------------------------------
+    #All of this is to just get the screen title b/c I don't know how to pass the variable before I call MainFrame
+    jsonFile = sys.argv[1]
+    if os.path.isfile(jsonFile):
+        infile = open(jsonFile, "r")
+        data = infile.read()
+        infile.close
+    else:
+        print("Error: File doesn't exist!")
+    playerData = json.loads(data)
+    title = "Player: " + playerData["screen_name"]
+    #--------------------------------------
+
+    
     # Create main window object
     root = tk.Tk()
     # Set title of window
-    root.title("Hello World!")
+    root.title(f"{title}")
     # Instantiate HelloWorldFrame object
-    hello_frame = HelloWorldFrame(root,"Nikola Tesla")
+    display = MainFrame(root)
     # Start GUI
-    hello_frame.mainloop()
+    display.mainloop()
